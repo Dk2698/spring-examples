0b991115e0baf16a6811c2ac8a073bc6101a27cb
Dk2698 <dileepkumar1823@gmail.com>
2023-04-25 10:26:52 +0530

    setup project

---
+++ src/test/java/com/kumar/springexamples/SpringExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumar.springexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
